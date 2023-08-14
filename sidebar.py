5802d770d474588ff0f2b4d720a2d30052fee0ff
arjunanil12 <63148901+arjunanil12@users.noreply.github.com>
2023-08-14 12:26:58 +0530

    Add files via upload

---
+++ sidebar.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def sidebar():
+    with st.sidebar:
+        st.markdown("# About")
+        st.markdown(" * We Digital and Emerging Technology as a team started exploring the world of Private, Public and Hybrid LLM Models.")
+        st.markdown(" * These cutting-edge approaches provide innovative solutions to the ever-growing challenges of data privacy and personalized natural language generation.")
+        st.markdown(" * Join us as we demystify these technologies and understand how they are revolutionizing the AI landscape. ")
---
+++ utils.py
@@ -0,0 +1,65 @@
+from sentence_transformers import SentenceTransformer
+import openai
+import streamlit as st
+
+#---------------------------
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import GPT4All, LlamaCpp
+import os
+import argparse
+import time
+
+load_dotenv()
+
+embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
+persist_directory = os.environ.get('PERSIST_DIRECTORY')
+
+model_type = os.environ.get('MODEL_TYPE')
+model_path = os.environ.get('MODEL_PATH')
+model_n_ctx = os.environ.get('MODEL_N_CTX')
+model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
+target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+
+from constants import CHROMA_SETTINGS
+
+#-------------------------------------------
+openai.api_key = "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"
+# openai.api_key = "sk-af571342449d40c69684ac6885948ed2"
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+def find_match(input):
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+    matching_docs = db.similarity_search(input)
+    matchtext=''
+    for indexVal, matchDoc in enumerate(matching_docs):
+        # print(matching_docs[indexVal].metadata['source'])
+        # print(matching_docs[indexVal].page_content)
+        matchtext +=matching_docs[indexVal].page_content
+    return matchtext
+
+def query_refiner(conversation, query):
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response['choices'][0]['text']
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
